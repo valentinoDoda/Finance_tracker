74da176e7e25e4732614fdbff2b6ba57aab6fece
unknown <valododa123@gmail.com>
2025-09-10 09:46:25 +0200

    Creating a form in profile page

--- app/pages/settings/profile.vue
+++ app/pages/settings/profile.vue
@@ -1,1 +1,34 @@
-<template>sdasd</template>
+<template>
+    <UForm :state="state" :schema="schema">
+    <UFormField class="mb-4" label="Full Name" name="name">
+      <UInput v-model="state.name" />
+    </UFormField>
+
+    <UFormField class="mb-4" label="Email" name="email"
+      help="You will receive a confirmation email on both the old and the new addresses if you modify the email address">
+      <UInput v-model="state.email" />
+    </UFormField>
+
+    <UButton type="submit" color="neutral" variant="solid" label="Save" :pending="pending" />
+  </UForm>
+</template>
+
+<script setup>
+import { z } from 'zod'
+
+//const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+console.log(user.value)
+//const { toastSuccess, toastError } = useAppToast()
+const pending = ref(false)
+
+const state = ref({
+  name: '',
+  email: user.value.email
+})
+
+const schema = z.object({
+  name: z.string().min(2).optional(),
+  email: z.string().email()
+})
+</script>
